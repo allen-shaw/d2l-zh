1534c34989f21b9f3ea838219b84d45312856d1e
Allen <allenshaw19@gmail.com>
2024-10-13 00:47:23 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from setuptools import setup, find_packages
 import d2l
 
 requirements = [
-    'jupyter==1.0.0',
-    'numpy==1.24.4',
-    'matplotlib==3.5.1',
-    'requests==2.25.1',
-    'pandas==1.4.4'
+    'jupyter',
+    'numpy',
+    'matplotlib',
+    'requests',
+    'pandas'
+    # 'jupyter==1.0.0',
+    # 'numpy==1.24.4',
+    # 'matplotlib==3.5.1',
+    # 'requests==2.25.1',
+    # 'pandas==1.4.4'
 ]
 
 setup(
